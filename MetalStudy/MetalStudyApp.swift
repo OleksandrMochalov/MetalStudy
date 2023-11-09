19b0b53cee0ad07d62d09e4770c8e5926cf2e73c
OleksandrMochalov <oleksandr.mochalov@monstar-lab.com>
2023-11-09 20:09:48 +0100

    Initial Commit

---
+++ MetalStudy/MetalStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalStudyApp.swift
+//  MetalStudy
+//
+//  Created by Oleksandr on 09.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MetalStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
